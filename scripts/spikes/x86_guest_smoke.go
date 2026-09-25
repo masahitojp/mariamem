5fd31b874abc8076631e50a8782c933b346c05da
masahitojp <randomstep@gmail.com>
2026-09-26 04:25:53 +0900

    ci: spike x86_64 WASIX guest build and macOS AOT smoke

---
+++ scripts/spikes/x86_guest_smoke.go
@@ -0,0 +1,43 @@
+// Architecture spike only: exercise a transferred WASM guest after macOS AOT.
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/masahitojp/mariamem"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("usage: x86_guest_smoke NATIVE_DIR")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+	db, err := mariamem.Start(ctx, mariamem.Options{NativeDir: os.Args[1]})
+	if err != nil {
+		panic(err)
+	}
+	pool, err := sql.Open("mysql", db.DSN())
+	if err != nil {
+		panic(err)
+	}
+	var one int
+	if err := pool.QueryRowContext(ctx, "SELECT 1").Scan(&one); err != nil {
+		panic(err)
+	}
+	if one != 1 {
+		panic(fmt.Sprintf("SELECT 1 returned %d", one))
+	}
+	if err := pool.Close(); err != nil {
+		panic(err)
+	}
+	if err := db.Close(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Start, SELECT 1, Close: PASS")
+}
